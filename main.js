0346c905688a4eed19892f0aa9a54a114dfd7f01
DaphnexxEliza <wanatchaya.idea@gmail.com>
2022-07-17 00:21:37 +0700

    3.5

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Shoes',
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
+            brand: 'SE331',
+            // image: './assets/images/socks_green.jpg',
+            // inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
             ],
-            cart: 0
+            cart: 0,
+            SelectedVariant: 0,
+            onSale: true
 
         }
     },
@@ -20,6 +23,29 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index) {
+            this.SelectedVariant = index;
         }
+
+    },
+    computed: {
+
+        title() {
+            return this.brand + ' ' + this.product
+        },
+
+        image() {
+            return this.variants[this.SelectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.SelectedVariant].quantity
+        },
+        onsalee() {
+            return this.brand + ' is on sale ' + this.product
+        }
+
+
     }
+
 })
