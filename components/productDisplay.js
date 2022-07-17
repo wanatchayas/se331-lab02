b00d348645296bba712f95d0f90cd3b041aea8c2
DaphnexxEliza <wanatchaya.idea@gmail.com>
2022-07-17 20:17:26 +0700

    6.6/6.7

--- components/productDisplay.js
+++ components/productDisplay.js
@@ -40,6 +40,7 @@ app.component('product-display', {
                 </button>
             </div>
         </div>
+        <review-list v-if="reviews.length" :reviews="reviews"></review-list>        
         <review-form @review-submited="addReview"></review-form>
     </div>`,
     data() {
